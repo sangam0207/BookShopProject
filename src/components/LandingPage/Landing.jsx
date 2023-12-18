c95fde372f8c251f35a3b708befc1842aa534b6c
sangam0207 <127228515+sangam0207@users.noreply.github.com>
2023-12-18 12:39:54 +0530

    Add files via upload

---
+++ src/components/LandingPage/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/Header'
+import BookList from '../BookList/BookList'
+function Landing() {
+  return (
+    <div>
+      <Header/>
+      <BookList/>
+    </div>
+  )
+}
+
+export default Landing
