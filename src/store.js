f72ce05606bff9eb6a7f4250840ade2cf68350f7
sangam0207 <127228515+sangam0207@users.noreply.github.com>
2023-12-18 12:23:50 +0530

    Delete src directory

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import cartReducer from './features/cartReducer';
-export const store = configureStore({
-  reducer: {
-    cartState: cartReducer,
-  },
-});
