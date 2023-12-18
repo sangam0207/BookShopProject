fe2822530b3b1532d2d01913bdb1a64acc4455b9
sangam0207 <127228515+sangam0207@users.noreply.github.com>
2023-12-18 12:44:21 +0530

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from '../../components/LandingPage/Landing';
+
+const Home = () => {
+  return (
+    <main>
+        <Landing/>
+    </main>
+  )
+}
+
+export default Home
